0074a98742c55ff0eceb4cd3007c72d69f1fd48d
Marvin <okongomarvin971@gmail.com>
2024-02-29 21:30:52 +0300

    Completed assignment

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+       print(i)
+       i -= 1
+    print("Happy New Year!")
+       
+happy_new_year()
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared = [ ints * ints for ints in int_list]
+    return squared
+    
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if (i%3 == 0 and i%5 == 0):
+            print("FizzBuzz")
+        elif i%3 == 0:
+            print("Fizz")
+        elif i%5 == 0:
+            print("Buzz")
+        else:
+            print(i)
